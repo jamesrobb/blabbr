1f8dc45bcbd43425132e8bafa95d7a0f266f8e6a
James Robb <me@jamesrobb.ca>
2016-11-06 22:22:21 +0000

    some fancy coloring for usernames in the chat area

--- src/chat_ui.h
+++ src/chat_ui.h
@@ -4,16 +4,22 @@
 #include <glib.h>
 #include <ncursesw/curses.h>
 #include <curses.h>
+#include <wchar.h>
 
 #include "line_buffer.h"
 
 #define NC_UNUSED(x) (void)(x)
 
-#define HEADER_BG_PAIR	2
-#define HEADER_FG_PAIR 	3
-#define WARNING_PAIR	7
+#define HEADER_BG_PAIR		2
+#define HEADER_FG_PAIR 		3
+#define WARNING_PAIR		7
+#define SERVER_USER_PAIR	8
+#define OTHER_USER_PAIR		6
+#define CURRENT_USER_PAIR	10
 
-#define SIDEBAR_WIDTH	15
+#define SIDEBAR_WIDTH		15
+
+extern wchar_t *ui_username;
 
 void gui_clear_all();
 
